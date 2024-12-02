7247936a6c895a064c37351e25fb314c50c647dd
Francesco_Rubino <frarubino00@gmail.com>
2024-12-02 16:01:30 +0100

    controllo dei numeri di Francesco

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 import class_file as cf 
 
-Francesco = cf.contatto ("Francesco" , "rubino" , "3311498928" , "09987676565")
+Francesco = cf.contatto ("Francesco" , "rubino" , "3311498928" , "0998767656")
 Francesco.stampa_contatto ()
 
 Claudia = cf.contatto ("CLaudia" , "Rubino" , "366336578" , "09987676565")
 Claudia.stampa_contatto ()
+
+Francesco.ceck_len_num ()
