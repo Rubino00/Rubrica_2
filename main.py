10d070ef42d0f6850e9b456836b77fb6f561b149
Francesco_Rubino <frarubino00@gmail.com>
2024-12-02 15:59:42 +0100

    aggiunto nuovo contatto: Claudia Rubino

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
 import class_file as cf 
 
-Francesco = cf.contatto ("Francesco" , "rubino" , "3311498928" , "09987676554")
+Francesco = cf.contatto ("Francesco" , "rubino" , "3311498928" , "09987676565")
 Francesco.stampa_contatto ()
+
+Claudia = cf.contatto ("CLaudia" , "Rubino" , "366336578" , "09987676565")
+Claudia.stampa_contatto ()
