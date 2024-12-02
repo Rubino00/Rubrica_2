22922a679adf5527176976c9ac45ecfbeb93dda0
Francesco_Rubino <frarubino00@gmail.com>
2024-12-02 16:03:03 +0100

    contrallati i numeri di Claudia

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import class_file as cf
 Francesco = cf.contatto ("Francesco" , "rubino" , "3311498928" , "0998767656")
 Francesco.stampa_contatto ()
 
-Claudia = cf.contatto ("CLaudia" , "Rubino" , "366336578" , "09987676565")
+Claudia = cf.contatto ("CLaudia" , "Rubino" , "3663365789" , "0998767656")
 Claudia.stampa_contatto ()
 
 Francesco.ceck_len_num ()
+Claudia.ceck_len_num ()
