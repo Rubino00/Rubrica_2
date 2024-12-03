44c9be9bc1c5af68663f79e60c3436fe23c8ed53
Francesco_Rubino <frarubino00@gmail.com>
2024-12-03 21:05:11 +0100

    aggiunto nuovo contatto: Fernando Rubino

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ Claudia.stampa_contatto ()
 
 Francesco.ceck_len_num ()
 Claudia.ceck_len_num ()
+
+Fernando = cf.contatto ("Fernando" , "Rubino" , "342567356" , "09987676565")
+Fernando.stampa_contatto ()
